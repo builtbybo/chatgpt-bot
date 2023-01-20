8a387142de85016ce3f23a90e4ef0cc95e53dde4
Bo Ajayi-Walmsley <bo.ajayi-walmsley@mkodo.com>
2023-01-20 23:44:41 +0000

    updated server url in script

--- client/script.js
+++ client/script.js
@@ -78,7 +78,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server
-  const response = await fetch('http://localhost:4000', {
+  const response = await fetch('https://chatgpt-bot-u3j9.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
